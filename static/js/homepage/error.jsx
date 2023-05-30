5e1ae6be6171bdf20da7c50070f066f9499b7cd4
Emiko Bell <emikoelizabethbell@gmail.com>
2023-05-30 10:35:15 -0700

    Add error.jsx

---
+++ static/js/homepage/error.jsx
@@ -0,0 +1,14 @@
+const ShowError = () => {
+    return (
+        <React.Fragment>
+            <ReactBootstrap.Row className="justify-content-center">
+                <ReactBootstrap.Col xs="auto" className="p-5">
+                    <h1>Error :&#40;</h1>
+                    <p>
+                        There was an error. Please reload the page and try again.
+                    </p>
+                </ReactBootstrap.Col>
+            </ReactBootstrap.Row>
+        </React.Fragment>
+    )
+};
