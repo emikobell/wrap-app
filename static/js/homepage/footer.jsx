6b2da5bfd073d46717c89c29fb5095c3bb1d58de
Emiko Bell <emikoelizabethbell@gmail.com>
2023-05-31 10:19:03 -0700

    Update footer

--- static/js/homepage/footer.jsx
+++ static/js/homepage/footer.jsx
@@ -2,18 +2,18 @@ const RenderFooter = () => {
     return (
         <ReactBootstrap.Container id="footer">
             <footer className="justify-content-between align-items-center py-3 my-4 border-top">
-                <ReactBootstrap.Col className="align-items-center">
-                    <ReactBootstrap.Nav md={4} className="justify-content-start">
-                        <ReactBootstrap.Nav.Item>
-                            <small className="text-muted mb-2">
-                            <i className="bi bi-code-slash"></i>
-                            &nbsp; with &nbsp;
-                            <i className="bi bi-heart-fill"></i>
-                            &nbsp; by Emiko Bell
+                <ReactBootstrap.Navbar>
+                    <ReactBootstrap.Container>
+                        <ReactBootstrap.Navbar.Brand href="#">
+                            <small className="text-muted mb-2 fs-6">
+                                <i className="bi bi-code-slash"></i>
+                                &nbsp; with &nbsp;
+                                <i className="bi bi-heart-fill"></i>
+                                &nbsp; by Emiko Bell
                             </small>
-                        </ReactBootstrap.Nav.Item>
-                    </ReactBootstrap.Nav>
-                    <ReactBootstrap.Nav md={4} className="justify-content-end">
+                        </ReactBootstrap.Navbar.Brand>
+                    </ReactBootstrap.Container>
+                    <ReactBootstrap.Nav className="ms-auto">
                         <ReactBootstrap.Nav.Item>
                             <ReactBootstrap.Nav.Link href="https://github.com/emikobell/wrap-app">
                                 <i class="bi bi-github"></i>
@@ -25,7 +25,7 @@ const RenderFooter = () => {
                             </ReactBootstrap.Nav.Link>
                         </ReactBootstrap.Nav.Item>
                     </ReactBootstrap.Nav>
-                </ReactBootstrap.Col>
+                </ReactBootstrap.Navbar>
             </footer>
         </ReactBootstrap.Container>
     )};
